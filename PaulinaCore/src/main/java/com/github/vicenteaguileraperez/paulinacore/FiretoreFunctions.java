eea3eee22a4df0a13a4437a9b3e7c1afec7ad7d6
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2020-12-22 11:27:24 -0600

    First Release

--- PaulinaCore/src/main/java/com/github/vicenteaguileraperez/paulinacore/FiretoreFunctions.java
+++ PaulinaCore/src/main/java/com/github/vicenteaguileraperez/paulinacore/FiretoreFunctions.java
@@ -6,6 +6,7 @@ import com.github.vicenteaguileraperez.paulinacore.enums.Options;
 import com.github.vicenteaguileraperez.paulinacore.interfaces.Something;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.Map;
 
@@ -19,6 +20,7 @@ import java.util.Map;
 public class FiretoreFunctions
 {
     private Something something;
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public void setListenerAny(Something something)
     {
@@ -26,7 +28,7 @@ public class FiretoreFunctions
     }
     public void addData(String collection, String document, Map<String,Object> data)
     {
-        FirebaseFirestore.getInstance().collection(collection).document(document)
+        db.collection(collection).document(document)
             .set(data)
             .addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
@@ -44,7 +46,7 @@ public class FiretoreFunctions
     }
     public void updateData(String collection, String document, Map<String, Object> data)
     {
-        FirebaseFirestore.getInstance().collection(collection).document(document)
+        db.collection(collection).document(document)
                 .update(data)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -61,7 +63,7 @@ public class FiretoreFunctions
     }
     public void deleteData(String collection,String document)
     {
-        FirebaseFirestore.getInstance().collection(collection).document(document)
+        db.collection(collection).document(document)
             .delete()
             .addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
