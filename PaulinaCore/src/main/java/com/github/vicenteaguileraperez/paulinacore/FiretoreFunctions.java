0958047ee5d938224e871e8ff3c1ebf5f47cf622
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2021-04-22 17:03:55 -0500

    Lanzando release 1.0.1

---
+++ PaulinaCore/src/main/java/com/github/vicenteaguileraperez/paulinacore/FiretoreFunctions.java
@@ -0,0 +1,81 @@
+package com.github.vicenteaguileraperez.paulinacore;
+
+import androidx.annotation.NonNull;
+
+import com.github.vicenteaguileraperez.paulinacore.enums.Options;
+import com.github.vicenteaguileraperez.paulinacore.interfaces.Something;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
+
+/**
+ * @author Vicente Aguilera Pérez
+ * @version 1.0.0
+ * Date December 18th, 2020
+ */
+
+public class FiretoreFunctions
+{
+    private Something something;
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void setListenerAny(Something something)
+    {
+        this.something=something;
+    }
+    public void addData(String collection, String document, Map<String,Object> data)
+    {
+        db.collection(collection).document(document)
+            .set(data)
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid)
+                {
+                    something.status(Options.ADD,true);
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    something.status(Options.ADD,false);
+                }
+            });
+    }
+    public void updateData(String collection, String document, Map<String, Object> data)
+    {
+        db.collection(collection).document(document)
+                .update(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        something.status(Options.UPDATE,true);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        something.status(Options.UPDATE,false);
+                    }
+                });
+    }
+    public void deleteData(String collection,String document)
+    {
+        db.collection(collection).document(document)
+            .delete()
+            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    something.status(Options.DELETE,true);
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    something.status(Options.DELETE,false);
+                }
+            });
+    }
+}
